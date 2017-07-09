f5b2320d74ad28d47c02110752512c7f148d3d45
gitmarroyo <miguel.marroyo@yahoo.es>
2017-07-09 20:07:16 +0200

    Segunda versión de mi programa

--- src/Nombres/MainClase.java
+++ src/Nombres/MainClase.java
@@ -2,7 +2,9 @@ package Nombres;
 
 public class MainClase {
 	public static void main(String[] args) {
-		Persona yo = new Persona("John Smith", 25);
+		Persona yo = new Persona("John Smith Marroyo", 25);
+		@SuppressWarnings("unused")
+		Persona tu = new Persona("Peter Smith Marroyo", 25);
 		yo.mostrarDatos(); // yo.edad = 25; da error
 	}
 }
