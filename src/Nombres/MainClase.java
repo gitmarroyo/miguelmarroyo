8cfb764033e184a6fe71f1eefc06b9b6f718d5e9
gitmarroyo <miguel.marroyo@yahoo.es>
2017-07-25 10:31:07 +0200

    Change-Id: I95840734da607bd4c2f93aff19ca2da6274ad513

--- src/Nombres/MainClase.java
+++ src/Nombres/MainClase.java
@@ -5,6 +5,7 @@ public class MainClase {
 		Persona yo = new Persona("John Smith Marroyo", 27);
 		@SuppressWarnings("unused")
 		Persona tu = new Persona("Peter Smith Marroyo", 26);
-		yo.mostrarDatos(); // yo.edad = 25; da error
+		yo.mostrarDatos(); // 
+		tu.mostrarDatos(); // 
 	}
 }
